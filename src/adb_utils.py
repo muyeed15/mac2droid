72858385e9729211bf5ce50ec4e36d107d7d79ff
Syed Abdullah Al Muyeed <2230324@iub.edu.bd>
2024-07-26 23:45:16 +0600

    chore: Add adb utility functions for device connection and management

--- src/adb_utils.py
+++ src/adb_utils.py
@@ -5,3 +5,9 @@ def fileList(target):
         target = f"/{target}"
 
     return os.popen(f'adb shell ls -a "/storage/self{target}"').read().splitlines()
+
+def connectDevice(ip, code):
+    return os.popen(f'adb "{ip}" "{code}"')
+
+def activeDevice():
+    return os.popen('adb device')
